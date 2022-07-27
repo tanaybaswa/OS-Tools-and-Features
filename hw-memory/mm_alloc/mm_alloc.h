9f47328481aed8142ebc717f3d6b1e4e24dcaffa
Tanay Baswa <tbaswa2212@berkeley.edu>
2022-07-27 22:13:34 +0000

    m10

--- hw-memory/mm_alloc/mm_alloc.h
+++ hw-memory/mm_alloc/mm_alloc.h
@@ -15,13 +15,4 @@ void* mm_malloc(size_t size);
 void* mm_realloc(void* ptr, size_t size);
 void mm_free(void* ptr);
 
-typedef struct block{
-    int size;
-    int free;
-    struct block *next;
-    struct block *prev;
-    char ptr[];
-
-} Block ;
-
 #endif
